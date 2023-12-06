640d10d525c691a785c6b9cad90d80ea83b2ab54
AltafKathathub2 <141987169+AltafKathathub2@users.noreply.github.com>
2023-12-06 15:01:56 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -6,14 +6,35 @@
 
 import dotenv from "dotenv";
 import dbconnect from "./db/dbconnect.js";
+import express from "express";
+import cors from "cors";
+// import cookieParser from "cookie-parser";
+
+const app = express();
+
 
 dotenv.config();
+app.use(cors({
+    origin: process.env.CORS
+}));
 
 
 
 
 
-dbconnect();
+
+dbconnect()
+  .then(() => {
+    const port = process.env.PORT || 8888;
+
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("ERROR: Database connection failed...", error);
+  });
+
 
 
 
